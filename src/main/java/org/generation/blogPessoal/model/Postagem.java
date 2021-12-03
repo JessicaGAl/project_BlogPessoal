50f5fa22bf3781571a6fd1f373a75adf83233677
unknown <jessyg19@hotmail.com>
2021-12-03 16:46:52 -0300

    @JessicaGAl [Task: Update]

--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -10,6 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,11 +25,11 @@ public class Postagem {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
+	@NotBlank
 	@Size (min = 5, max = 100)
 	private String titulo;
 	
-	@NotNull
+	@NotBlank
 	@Size (min = 10, max = 500)
 	private String texto;
 	
--- src/main/java/org/generation/blogPessoal/model/Tema.java
+++ src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -25,7 +26,7 @@ public class Tema {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-    @NotNull
+    @NotBlank
 	private String descricao;
 	
 	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
