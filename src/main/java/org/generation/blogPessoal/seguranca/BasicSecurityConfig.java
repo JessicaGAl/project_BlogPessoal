985c581e65aedbd891971bf445d7ea1e341ded59
unknown <jessyg19@hotmail.com>
2021-12-13 16:53:08 -0300

    @JessicaGAl [Task: Security]

---
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -0,0 +1,42 @@
+package org.generation.blogPessoal.seguranca;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{ 
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override 
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+		
+	}
+	@Bean 
+	public PasswordEncoder passwaordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
+	@Override 
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()                                        // autorizar a rota que vai passar
+		.antMatchers("/usuarios/logar.").permitAll()    //permissão para rota qualquer um = All  
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()  // resposta HTTP
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // entrada por sessão
+		.and().cors()     // 
+		.and().csrf().disable();  
+	}
+
+}
