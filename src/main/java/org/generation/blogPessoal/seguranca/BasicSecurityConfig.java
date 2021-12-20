b4f515a7e336902c08b065f822a41e99c41a40e1
unknown <jessyg19@hotmail.com>
2021-12-20 12:43:46 -0300

    @JessicaGAl [Task: Update Password]

--- src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -21,7 +21,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override 
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
-			auth.inMemoryAuthentication().withUser("jess").password(passwaordEncoder().encode("041993"))
+			auth.inMemoryAuthentication().withUser("root").password(passwaordEncoder().encode("root"))
 					.authorities("ROLE_ADMIN");
 		}
 		
